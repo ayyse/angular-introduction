1272d2642eec537e718f4ff2abef4dc598f68de0
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2021-05-16 15:20:03 +0300

    ders-18

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,15 +12,28 @@ export class ProductComponent implements OnInit {
   products:Product[] = [];
   dataLoaded = false;
   
-  constructor(private productService : ProductService) {}
+  constructor(private productService : ProductService, private activatedRoute: ActivatedRoute) {}
 
   // ProductComponent ilk açıldığında çalışan metod
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["categoryId"]){
+        this.getProductsByCategory(params["categoryId"])
+      }else{
+        this.getProducts()
+      }
+    })
   }
 
   getProducts(){
     this.productService.getProducts().subscribe(response => {
+      this.products = response.data
+      this.dataLoaded = true;
+    })
+  }
+
+  getProductsByCategory(categoryId: number){
+    this.productService.getProductsByCategory(categoryId).subscribe(response => {
       this.products = response.data
       this.dataLoaded = true;
     })
